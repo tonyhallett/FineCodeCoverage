4251c073604e8ec7e6ec92a231484452bb3f1b70
Tony Hallett <tonyhallett74@gmail.com>
2024-03-29 09:24:23 +0000

    renaming png

--- SharedProject/Output/ReportToolWindow.cs
+++ SharedProject/Output/ReportToolWindow.cs
@@ -41,7 +41,6 @@ namespace FineCodeCoverage.Output
 
             this.Caption = Vsix.Name;
             this.BitmapImageMoniker = Microsoft.VisualStudio.Imaging.KnownMonikers.CodeCoverage;
-
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
