8ef157fbb4a699bf2be5b58be8ad73d09773648b
Tony Hallett <tonyhallett74@gmail.com>
2024-03-31 13:06:35 +0100

    separate file for class

---
+++ SharedProject/Core/Utilities/FCCVersioning/FCCVersion.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+
+namespace FineCodeCoverage.Core.Utilities.FCCVersioning
+{
+    [Export(typeof(IFCCVersion))]
+    internal class FCCVersion : IFCCVersion
+    {
+        private string version;
+        [ImportingConstructor]
+        public FCCVersion()
+        {
+
+        }
+        public string GetVersion()
+        {
+            if (this.version == null)
+            {
+                this.SetVsixVersion();
+            }
+
+            return this.version;
+        }
+
+        private void SetVsixVersion()
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            string asmDir = Path.GetDirectoryName(asm.Location);
+            string manifestPath = Path.Combine(asmDir, "extension.vsixmanifest");
+            if (File.Exists(manifestPath))
+            {
+                var doc = new XmlDocument();
+                doc.Load(manifestPath);
+                XmlElement metaData = doc.DocumentElement.ChildNodes.Cast<XmlElement>().First(x => x.Name == "Metadata");
+                XmlElement identity = metaData.ChildNodes.Cast<XmlElement>().First(x => x.Name == "Identity");
+                this.version = identity.GetAttribute("Version");
+
+            }
+        }
+    }
+
+}
--- SharedProject/Core/Utilities/FCCVersioning/IFCCVersion.cs
+++ SharedProject/Core/Utilities/FCCVersioning/IFCCVersion.cs
@@ -1,49 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Xml;
-
-namespace FineCodeCoverage.Core.Utilities.FCCVersioning
+﻿namespace FineCodeCoverage.Core.Utilities.FCCVersioning
 {
-    [Export(typeof(IFCCVersion))]
-    internal class FCCVersion : IFCCVersion
-    {
-        private string version;
-        [ImportingConstructor]
-        public FCCVersion()
-        {
-
-        }
-        public string GetVersion()
-        {
-            if(this.version == null)
-            {
-                this.SetVsixVersion();
-            }
-
-            return this.version;
-        }
-
-        private void SetVsixVersion()
-        {
-            var asm = Assembly.GetExecutingAssembly();
-            string asmDir = Path.GetDirectoryName(asm.Location);
-            string manifestPath = Path.Combine(asmDir, "extension.vsixmanifest");
-            if (File.Exists(manifestPath))
-            {
-                var doc = new XmlDocument();
-                doc.Load(manifestPath);
-                XmlElement metaData = doc.DocumentElement.ChildNodes.Cast<XmlElement>().First(x => x.Name == "Metadata");
-                XmlElement identity = metaData.ChildNodes.Cast<XmlElement>().First(x => x.Name == "Identity");
-                this.version = identity.GetAttribute("Version");
-
-            }
-        }
-    }
     internal interface IFCCVersion
     {
         string GetVersion();
